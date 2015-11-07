bff6c43e0b594a46183dc8f330ba2aa557efc836
JoaoMosmann <s0adex@gmail.com>
2015-11-07 18:43:20 +0000

    Add header CSS - Add search-icon to sprites

--- frontend/app/js/components/ProfileWidget.jsx
+++ frontend/app/js/components/ProfileWidget.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router'
 export default class ProfileWidget extends React.Component {
 
   render () {
-    return <div>
+    return <div className="profile-widget">
       ProfileWidget
     </div>
   }
