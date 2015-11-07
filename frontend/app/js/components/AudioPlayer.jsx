b1d32b90d921aeed0fb388222611c75e29e76011
mdtarso <maurodetarso@gmail.com>
2015-11-07 21:15:03 +0000

    testing audio playback

--- frontend/app/js/components/AudioPlayer.jsx
+++ frontend/app/js/components/AudioPlayer.jsx
@@ -1,13 +1,47 @@
 import React from 'react'
 import { Link } from 'react-router'
+import Notifier from '../tools/Notifier';
 
 export default class AudioPlayer  extends React.Component {
+  constructor() {
+    this.state = {
+      url: null,
+      status: 'stopped',
+      position: 0
+    };
 
-  render () {
-    return <div className="audio-player">
-      AudioPlayer
-    </div>
+
+    Notifier.get('main').on('play', this.onAudioSelect.bind(this));
   }
 
-}
+  onAudioSelect(url) {
+    this.audio.src = url;
+    this.audio.autoPlay = true;
+  }
 
+  handleSongLoading () {
+
+  }
+
+  handleSongPlaying () {
+
+  }
+
+  handleSongFinishedPlaying () {
+
+  }
+
+  componentDidMount() {
+    this.audio = React.findDOMNode(this.refs.audio);
+  }
+
+  render () {
+    return (
+      <div className="audio-player">
+        <audio ref='audio' controls autoPlay>
+          <source src={this.state.url} />
+        </audio>
+      </div>
+    );
+  }
+}
