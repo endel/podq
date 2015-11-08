96153ee1a4257e2ffbb071b1d5ea608f8bfc7467
Endel Dreyer <endel.dreyer@gmail.com>
2015-11-08 21:43:49 +0000

    remove imagemin dependency (breaking svgs)

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -106,13 +106,6 @@ gulp.task('html', ['stylesheet'], function () {
 
 gulp.task('images', function () {
   return gulp.src('app/images/**/*')
-    .pipe($.cache($.imagemin({
-      progressive: true,
-      interlaced: true,
-      // don't remove IDs from SVGs, they are often used
-      // as hooks for embedding and styling
-      svgoPlugins: [{cleanupIDs: false}]
-    })))
     .pipe(gulp.dest('../public/images'));
 });
 
