e46876d7ef43f701330b56efda820d30f3f1a807
Endel Dreyer <endel.dreyer@gmail.com>
2016-05-02 20:00:29 +0100

    set default value on User#following

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User
   field :name, type: String
   field :email, type: String
   field :image, type: String
-  field :following, type: Array
+  field :following, type: Array, default: -> { [] }
 
   validates_presence_of :name
 
